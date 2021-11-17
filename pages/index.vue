7bcc6b94db11946ff1f1071105926e7bd2b05d20
Fabrizio Oddo <wookiefab@gmail.com>
2021-11-17 19:50:44 +0100

    Index fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,27 @@
 <template>
   <v-container fluid>
-      <v-card flat outlined elevation="7" class="py-4 d-flex justify-center">
-    <v-row justify="center" align="center">
-    
-      <v-col cols="12" sm="8" md="6" justify-space-between>
+     
+       <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6" justify-space-between>
         <v-card-title class="headline" justify="center" align="center">
           Latest News &nbsp; <a href="https://twitter.com/AdmiralBulldog?ref_src=twsrc%5Etfw" class="twitter-follow-button"  data-show-screen-name="false" data-show-count="false">Follow @AdmiralBulldog</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </v-card-title>
-        <v-card elevation="2"  justify="center" align="center">
+        <v-card elevation="4"  justify="center" align="center" style="height:34rem">
           <v-card-text>
             <LatestPost />
           </v-card-text>
         </v-card>
       </v-col>
-
       <v-col cols="12" sm="8" md="6" justify-space-between>
         <v-card-title class="headline" justify="center" align="center">
           Latest YouTube Videos
         </v-card-title>
         <v-card
-          elevation="2"
+          elevation="4"
           justify="center"
           align="center"
           id="ID_Youtube"
+          style="min-height:34rem"
         >
           <v-card-text>
             <!-- latest four videos -->
@@ -35,15 +34,16 @@
               allowfullscreen
               frameborder="0"
               :src="item.src"
-              style="margin:3%"
+              class="videobox"
             >
             </iframe>
           </v-card-text>
         </v-card>
       </v-col>
-    </v-row>    
-    </v-card>
+    </v-row>        
   </v-container>
+
+  
 </template>
 
 <script lang="ts">
@@ -70,3 +70,13 @@ export default Vue.extend({
   },
 });
 </script>
+
+
+<style scoped>
+  .videobox {
+    margin:2%;
+  }
+ .videobox:hover {
+    box-shadow: 0px 0px 15px 5px rgba(58,255,34,0.55);
+ }
+</style>
