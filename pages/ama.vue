f942d9a2befb05a146051072376467977be133f7
Fabrizio Oddo <f.oddo@me.com>
2022-01-01 14:15:42 +0100

    AMA Page init

---
+++ pages/ama.vue
@@ -0,0 +1,36 @@
+<template>
+<v-container fluid>
+  <v-row>
+    <v-card>
+        AMAs in here
+      </v-card>    
+  </v-row>
+</v-container>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style lang="scss" scoped>
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 2px grey;
+  border-radius: 10px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #0b6636;
+  border-radius: 10px;
+}
+
+</style>
