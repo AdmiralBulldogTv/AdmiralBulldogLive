ff4e0e078e08edd4d2d3fd3fb0ae31f692b73c0b
Fabrizio Oddo <wookiefab@gmail.com>
2022-01-27 08:20:25 +0100

    token

---
+++ config.js
@@ -0,0 +1,25 @@
+export default {
+  config : {
+    OAUTH_TOKEN : 'shunalufcrsacjoozekt99wbiggyu9',
+    CLIENT_ID : 'pe8j3m8aepe7wa1n4qvba6jvvatfzi',
+  },
+};
+
+/* 
+Client ID: pe8j3m8aepe7wa1n4qvba6jvvatfzi
+authorization: j92c0znya03rnfpdfh299nj9cvfhlb
+
+https://id.twitch.tv/oauth2/authorize?client_id=pe8j3m8aepe7wa1n4qvba6jvvatfzi&redirect_uri=http://localhost&response_type=code&scope=viewing_activity_read
+
+http://localhost/?code=j92c0znya03rnfpdfh299nj9cvfhlb&scope=viewing_activity_read
+
+
+
+oauth Implicit code flow
+https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=pe8j3m8aepe7wa1n4qvba6jvvatfzi&redirect_uri=http://localhost&scope=viewing_activity_read
+
+-->
+
+http://localhost/#access_token=shunalufcrsacjoozekt99wbiggyu9&scope=viewing_activity_read&token_type=bearer
+
+*/
