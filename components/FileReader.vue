37aac53056310eb02ae3d14774df9ebd06624bf6
Wintersun93 <63934430+Wintersun93@users.noreply.github.com>
2022-05-03 00:49:13 +0200

    Added AMA / Tierlists + Minor Layout Adjustments (#25)
    * Small Layout adjustments
    
    * Added new AMA & Tierlists
    
    * Added Images
    
    * Added Images
    
    * added settings.json
    
    * typo fix
    
    * typo fix

--- components/FileReader.vue
+++ components/FileReader.vue
@@ -72,6 +72,7 @@ const ama_210214: object = require("raw-loader!../assets/amas/ama_210214.txt");
 const ama_210119: object = require("raw-loader!../assets/amas/ama_210119.txt");
 const ama_210110: object = require("raw-loader!../assets/amas/ama_210110.txt");
 const ama_201129: object = require("raw-loader!../assets/amas/ama_201129.txt");
+const ama_220411: object = require("raw-loader!../assets/amas/ama_220411.txt");
 
 import Vue from "vue";
 export default Vue.extend({
@@ -80,6 +81,7 @@ export default Vue.extend({
       isTop: false,
       showNavBar: false,
       amas: [
+        { title: "AMA of 11.04.2022", file: ama_220411 },
         { title: "AMA of 13.02.2022", file: ama_220213 },
         { title: "AMA of 22.12.2021", file: ama_211222 },
         { title: "AMA of 09.10.2021", file: ama_211009 },
@@ -136,7 +138,7 @@ export default Vue.extend({
     },
   },
   mounted() {
-    this.getAma(ama_220213);
+    this.getAma(this.amas[0].file);
     self.document
       .getElementById("card_id")
       .addEventListener("scroll", this.onScroll);
